3d86329a42dfbb2e9ce65489695e661a1fbafa99
Jhonatan <jhonatanruivo@outlook.com>
2023-11-15 17:14:12 -0300

    Project started

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: ['prettier-plugin-tailwindcss'],
+trailingComma: 'es5',
+tabWidth: 2,
+semi: false,
+singleQuote: true,
+printWidth: 80,
+bracketSpacing: true,
+arrowParens: 'always',
+vueIndentScriptAndStyle: true,
+}
